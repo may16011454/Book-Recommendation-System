1c26ed1c7f6cdf76890eb0d41cde4e5f19a5b597
may16011454 <92788643+may16011454@users.noreply.github.com>
2023-06-12 13:59:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-## edit below variables as per your requirements -
-REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Books-Recommendation-System"
+AUTHOR_USER_NAME = "may16011454"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
